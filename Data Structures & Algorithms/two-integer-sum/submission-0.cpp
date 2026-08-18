356afab4c49c9f7610c509d677fc95fd4a4f9cfc
Purvi Solanki <87231001+Purvisolanki@users.noreply.github.com>
2026-08-18 19:59:08 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //         {
+        //           return {i,j};
+        //         }
+                
+        //     }
+        // }
+        //  return {};
+        unordered_map<int, int> mpp;
+    for (int i = 0; i < nums.size(); i++) {
+        int num = nums[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return { -1, -1};
+
+
+    }
+   
+};
+
